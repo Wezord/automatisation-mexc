8839266d914eb6ea7ef04feb5cb74faaa4600391
GRUCHY Aurelien <aurelien.gruchy@gmail.com>
2025-01-09 10:31:13 +0100

    Lancement du serveur host par ngrok à partir de main.py. On recoit les alertes TradingView mais aucun process derrière

---
+++ mexc-request.py
@@ -0,0 +1,42 @@
+import hashlib
+import json
+import time
+from curl_cffi import requests
+
+def md5(value):
+    return hashlib.md5(value.encode('utf-8')).hexdigest()
+
+def mexc_crypto(key, obj):
+    date_now = str(int(time.time() * 1000))  
+    g = md5(key + date_now)[7:] 
+    s = json.dumps(obj, separators=(',', ':'))  
+    sign = md5(date_now + s + g)  
+    return {'time': date_now, 'sign': sign}
+
+def place_order(key, obj, url):
+    signature = mexc_crypto(key, obj)
+    headers = {
+        'Content-Type': 'application/json',
+        'x-mxc-sign': signature['sign'],
+        'x-mxc-nonce': signature['time'],
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Authorization': key
+    }
+    response = requests.post(url, headers=headers, json=obj)
+    return response.json()
+key = 'WEB6a046ade52318bd36392ba06d44c65118f50871d8a59dbb52acd32bf07665628'
+obj = { 
+    "symbol": "BTC_USDT", 
+    "side": 1, 
+    "openType": 1, 
+    "type": "1", 
+    "vol": 170, 
+    "leverage": 20, 
+    "price": 2.5, 
+    "priceProtect": "0" 
+}
+
+url = 'https://futures.mexc.com/api/v1/private/order/create'
+
+response = place_order(key, obj, url)
+print(response)
