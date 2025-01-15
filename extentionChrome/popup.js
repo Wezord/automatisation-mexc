7ea83a5d4e7530138d0a24ac793874afcc1d813b
GRUCHY Aurelien <aurelien.gruchy@gmail.com>
2025-01-15 19:37:36 +0100

    pasfe

--- extentionChrome/popup.js
+++ extentionChrome/popup.js
@@ -1,3 +1,4 @@
+// Variables
 const dicStrats = {
   "x3" : 1,
   "rsi": 2,
@@ -6,105 +7,13 @@ const dicStrats = {
   "X3-test": 69356848,
   "Bollinge-Test": 69261661
 };
-const ngrokURL = "https://704e-83-202-127-170.ngrok-free.app"
+const ngrokURL = "https://2722-146-70-142-83.ngrok-free.app"
+
 var varStratSelect;
 var selectStrat;
+var selectQuantite;
 
-document.getElementById("changeUrlButton").addEventListener("click", () => {
-  console.log("test ?");
-
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs.length > 0) {
-      const currentTab = tabs[0];
-      const currentUrl = currentTab.url;
-
-      console.log("test");
-
-      // Construire la nouvelle URL avec la valeur 'actif'
-      const newUrl = "https://futures.mexc.com/fr-FR/exchange/SPELL_USDT?type=linear_swap";
-      console.log(newUrl); // Affiche l'URL modifiée
-
-      // Mettre à jour l'URL et forcer un rechargement
-      chrome.tabs.update(currentTab.id, { url: newUrl }, () => {
-        // Après la mise à jour de l'URL, forcer un rechargement de la page
-        console.log("reload " + newUrl);
-        //chrome.tabs.reload(currentTab.id);
-      });
-    } else {
-      console.error("Aucun onglet actif trouvé.");
-    }
-  });
-});
-
-document.getElementById("fillFormButton").addEventListener("click", () => {
-  console.log("hello world");
-  //let listElements = document.querySelectorAll("#mexc_contract_v_open_position .ant-input");
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const currentTab = tabs[0];
-        const currentUrl = currentTab.url;
-  
-        // Injecter un script pour modifier l'URL de la page
-      chrome.scripting.executeScript({target: { tabId: currentTab.id },func: (url) => {
-          const listElements = document.querySelectorAll("#mexc_contract_v_open_position .ant-input");
-          console.log("listElements:", listElements);
-          element=listElements[0];
-          console.log("listElements:", listElements);
-          console.log("Elements:", element);
-          
-          if (!element){
-              alert("Aucun élément avec la classe 'maClasse' trouvé dans l'élément avec ID 'monId'.");
-          }
-          else{
-              //element.value="9495";
-              element.click();
-              element.value="98";
-
-              // Simuler un événement 'input' pour indiquer que la valeur a changé
-              const inputEvent = new Event("input", { bubbles: true });
-              element.dispatchEvent(inputEvent);
-
-              // Optionnel : Simuler un événement 'change' si nécessaire
-              const changeEvent = new Event("change", { bubbles: true });
-              element.dispatchEvent(changeEvent);
-      
-              alert("Texte inséré (enfin normalement)");
-          }
-      }
-      });
-  }
-  });     
-});
-
-/*document.getElementById("hoverElement").addEventListener("click", () => {
-  console.log("hello world");
-  //let listElements = document.querySelectorAll("#mexc_contract_v_open_position .ant-input");
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const currentTab = tabs[0];
-        const currentUrl = currentTab.url;
-  
-        // Injecter un script pour modifier l'URL de la page
-      chrome.scripting.executeScript({target: { tabId: currentTab.id },func: (url) => {
-        const element = document.querySelector('.ant-col'); // Remplace '.element-cible' par le sélecteur de ton élément
-        console.log(element);
-        element.click();
-        // Simuler l'événement 'mouseenter' (le survol de l'élément)
-        const simulateMouseEnter = () => {
-          console.log("survol");
-            const event = new MouseEvent('mouseenter', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            });
-            element.dispatchEvent(event);
-        };
-      }
-      });
-  }
-  });     
-});*/
-
+// Boucle récursive pour récupérer les alertes
 async function infiniteTrade(strat_to_use = "alert"){
   const strat = strat_to_use;
   const url = ngrokURL + "/" + strat;
@@ -141,39 +50,24 @@ async function infiniteTrade(strat_to_use = "alert"){
   infiniteTrade(strat);
 }
 
+// Clique sur démarrer le Bot
 let strategies = [];
 document.getElementById("sendRequest").addEventListener("click", async () => {
+
+  //preventDefault(); // Empêcher le rechargement de la page
+  // Récupérer la clé sélectionnée
+  const selectedKey = selectElement.value;
+  selectStrat = selectedKey;
+  selectQuantite = inputElement.value;
+
+  // Trouver la valeur correspondante dans le dictionnaire
+  const value = dicStrats[selectedKey];
+  varStratSelect=value;
+
+  // Afficher la valeur dans l'élément <p>
+  resultElement.textContent = `Valeur sélectionnée : ${value}`;
+
   infiniteTrade(selectStrat);
-});
-
-document.getElementById("clickButton").addEventListener("click", () => {
-  console.log("hello world");
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const currentTab = tabs[0];
-        const currentUrl = currentTab.url;
-  
-        // Injecter un script pour modifier l'URL de la page
-      chrome.scripting.executeScript({target: { tabId: currentTab.id },func: (url) => {
-          const listElements = document.querySelectorAll(".ant-checkbox-input");
-          element=listElements[2];
-          
-          if (!element){
-              alert("Aucun élément avec la classe 'maClasse' trouvé dans l'élément avec ID 'monId'.");
-          }
-          else{
-              element.click();
-
-              // Optionnel : Simuler un événement 'change' si nécessaire
-              const changeEvent = new Event("change", { bubbles: true });
-              element.dispatchEvent(changeEvent);
-      
-              alert("Button cliqué");
-          }
-      }
-      });
-  }
-  });     
 });
 
 ///////////////////////////////////////////////////
@@ -183,6 +77,7 @@ document.getElementById("clickButton").addEventListener("click", () => {
 const selectElement = document.getElementById("choixStrategies");
 const resultElement = document.getElementById("choixStratResult");
 const formElement = document.getElementById("strategiesForm");
+const inputElement = document.getElementById("inputQuantite");
 
 // Remplir la liste déroulante avec les clés du dictionnaire
 function populateSelectOptions() {
@@ -208,43 +103,8 @@ function demandeChangementUtilisateur(data) {
   });
 }
 
-// Gérer la soumission du formulaire
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault(); // Empêcher le rechargement de la page
-
-  // Récupérer la clé sélectionnée
-  const selectedKey = selectElement.value;
-  selectStrat = selectedKey;
-
-  // Trouver la valeur correspondante dans le dictionnaire
-  const value = dicStrats[selectedKey];
-  varStratSelect=value;
-
-  // Afficher la valeur dans l'élément <p>
-  resultElement.textContent = `Valeur sélectionnée : ${value}`;
-});
-
 // Initialiser la liste déroulante
 populateSelectOptions();
-
-document.getElementById("changeAccount").addEventListener("click", async () => {
-  const url = "https://www.mexc.co/fr-FR/user/switch-account";
-  const stratSelect = varStratSelect; // Assure-toi que cette variable est définie
-  
-  //alert("Valeur de stratSelect :"+ stratSelect);
-  //attendre(2000);
-  demandeChangementUtilisateur(stratSelect);
-  // Ouvrir un nouvel ongletC
-  chrome.tabs.create({ url: url }, async (tab) => {
-    console.log("Nouvel onglet ouvert :", tab);
-
-    // Attendre 10 secondes pour que la page charge (augmente si nécessaire)
-    await attendre(10000);
-
-    // Injecter le code dans le nouvel onglet
-    
-  });
-});
 
 /////////////////////////////////////////////////////////////////
 //////////////FIN CHANGEMENT COMPTE//////////////////////////////
@@ -260,22 +120,23 @@ async function process_alert(alerte){
       const nomActif = element["actif"].split(".")[0];
       const position = element.position;
       const type = element.type;
-      const stopLoss = parseInt(element.stopLoss, 10);
-      const valueStopLoss = parseFloat(element.alert_message, 10);;
-      const quantite = 90;
-      console.log(nomActif + " " + position + " " + type +" "  + element.strategy_order_name);
+      const stopLoss = parseInt(element.stop_loss, 10);
+      const valueStopLoss = parseFloat(element.alert_message, 10);
+      console.log(nomActif + " " + position + " " + type +" "  + element.strategy_order_name + " " + stopLoss + " " + valueStopLoss  + " ");
       // Change l'url
       delete_alert(element);
-      await attendre(2000);
-      await searchCrypto(nomActif);
       //changementURL2("https://futures.mexc.com/fr-FR/exchange/" + nomActif + "_USDT?type=linear_swap");
       await attendre(3000);
       // Achete au long
       if(position == "short" && type == "buy"){
-        await buy_short(stopLoss, valueStopLoss, 90);
+        await searchCrypto(nomActif);
+        await attendre(500);
+        await buy_short(stopLoss, valueStopLoss, selectQuantite);
       }
       else if (position == "long" && type == "buy"){
-        await buy_long(stopLoss, valueStopLoss, 90);
+        await searchCrypto(nomActif);
+        await attendre(500);
+        await buy_long(stopLoss, valueStopLoss, selectQuantite);
       }
       else if (position == "short" && type == "sell"){
         closeTrade(nomActif, "short");
@@ -300,6 +161,7 @@ async function process_alert(alerte){
   }
   return alerte;
 }
+
 
 async function delete_alert(alerte_to_delete){
   const url = ngrokURL + "/delete_alert";
@@ -498,42 +360,15 @@ async function buy(valeur,stopLoss=0,takeProfit=0,long=true){
   console.log("ordre réalisé");
 }
 
-async function close_short(){
-  click_button(".handle_vInner__IXFRx",0,true);
-  await attendre(2000);
-  click_button(".InputNumberExtend_wrapper__qxkpD .ant-input", 1);
-  await attendre(2000);
-  fillButton(".InputNumberExtend_wrapper__qxkpD .ant-input", 1, 100000000);
-  await attendre(2000);
-  click_button(".component_longBtn__BBkFR", 1);
-  await attendre(2000);
-  console.log("ordre réalisé");
-}
-
-async function close_long(){
-  click_button(".handle_vInner__IXFRx",0,true);
-  await attendre(2000);
-  click_button(".InputNumberExtend_wrapper__qxkpD .ant-input", 1);
-  await attendre(2000);
-  fillButton(".InputNumberExtend_wrapper__qxkpD .ant-input", 1, 100000000);
-  await attendre(2000);
-  click_button(".component_shortBtn__s8HK4", 1);
-  await attendre(2000);
-  console.log("ordre réalisé");
-}
-
-
 async function searchCrypto(actif){
   click_button(".contractDetail_contractNameBox__IcVlT", 0);
   await attendre(1000);
   click_button(".ant-input", 2);
-  await attendre(1000);
+  await attendre(500);
   fillButton(".ant-input", 2, actif);
   await attendre(1000);
   click_button(".Pairs_row__XKonK", 1);
   await attendre(2000);
-  //click_button(".contractDetail_contractNameBox__IcVlT", 0);
-  await attendre(1000);
 }
 
 function attendre(ms) { 
@@ -542,7 +377,7 @@ function attendre(ms) {
 
 function closeTrade(crypto,long){//crypto: les deux ou trois lettre majuscules qui définissent une crypto ex: BTC, ETH etc
   //Long c'est simplement un bouléen qui va nous indiquer quel type de position doit être fermé
-  crypto=crypto+"USDT";
+  //crypto=crypto+"USDT";
   console.log(crypto);
   class_component=".ant-table-row-level-0";
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
